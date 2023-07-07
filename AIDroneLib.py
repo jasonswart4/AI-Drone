625668f0b2a4d7a6079aa5cb8d765e7e16bf1ef4
jason.swart <jasonswart4@gmail.com>
2023-07-08 09:35:52 +1000

    Code that kinda works but needs a lot of manipulation of mutation rates during run

--- AIDroneLib.py
+++ AIDroneLib.py
@@ -1,237 +1,205 @@
 import tensorflow.keras as tf
 import random
 import numpy as np
-import copy
 import pygame
 import matplotlib.pyplot as plt
+
 
 def createNet(structure, activF):
     name = tf.Sequential()
     name.add(tf.layers.Input(shape=(structure[0])))
-    for i in range(len(structure)-1):
-        name.add(tf.layers.Dense(structure[i+1], activation = activF))
+    for i in range(len(structure) - 1):
+        name.add(tf.layers.Dense(structure[i + 1], activation=activF))
     return name
 
-def mutateNet(net,r):
+
+def mutateNet(net, r):
     w = net.get_weights()
     w_new = w
-    for i in np.arange(0,len(w)-1,2):
-        for j in range(len(w[i][:,0])):
-            for k in range(len(w[i][0,:])):
-                #w_new[i][j,k] = np.random.choice([w[i][j,k],w[i][j,k]+random.uniform(-.5,.5)],1,p=[1-r,r])
-                #w_new[i][j,k] = np.random.choice([w[i][j,k], np.random.randn(), w[i][j,k]+random.uniform(-.5,.5)], 1, p=[1-r,r*1/10,r*9/10])
-                w_new[i][j,k] = np.random.choice([w[i][j,k], w[i][j,k]+random.uniform(-.5,.5)], 1, p=[1-r,r])
+    for i in np.arange(0, len(w) - 1, 2):
+        for j in range(len(w[i][:, 0])):
+            for k in range(len(w[i][0, :])):
+                # w_new[i][j,k] = np.random.choice([w[i][j,k],w[i][j,k]+random.uniform(-.5,.5)],1,p=[1-r,r])
+                # w_new[i][j,k] = np.random.choice([w[i][j,k], np.random.randn(), w[i][j,k]+random.uniform(-.5,.5)], 1, p=[1-r,r*1/10,r*9/10])
+                w_new[i][j, k] = np.random.choice([w[i][j, k], w[i][j, k] + random.uniform(-.5, .5)], 1, p=[1 - r, r])
 
     mutant = tf.models.clone_model(net)
     mutant.set_weights(w_new)
     return mutant
 
-def mutateBias(net,r):
+'''
+def mutateBias(net, r):
     w = net.get_weights()
     w_new = w
-    for i in np.arange(1,len(w),2):
+    for i in np.arange(1, len(w), 2):
         for j in range(len(w[i])):
-           
-            #w_new[i][j] = np.random.choice([w[i][j,k],w[i][j,k]+random.uniform(-.5,.5)],1,p=[1-r,r])
-            w_new[i][j] = np.random.choice([w[i][j], w[i][j]+random.uniform(-.5,.5)],1,p=[1-r,r])
+            # w_new[i][j] = np.random.choice([w[i][j,k],w[i][j,k]+random.uniform(-.5,.5)],1,p=[1-r,r])
+            w_new[i][j] = np.random.choice([w[i][j], w[i][j] + random.uniform(-.5, .5)], 1, p=[1 - r, r])
 
     mutant = tf.models.clone_model(net)
     mutant.set_weights(w_new)
     return mutant
-
 def set0(net):
     w = net.get_weights()
     w_new = w
-    for i in np.arange(0,len(w)-1,2):
-        for j in range(len(w[i][:,0])):
-            for k in range(len(w[i][0,:])):
-                w_new[i][j,k] = 0
+    for i in np.arange(0, len(w) - 1, 2):
+        for j in range(len(w[i][:, 0])):
+            for k in range(len(w[i][0, :])):
+                w_new[i][j, k] = 0
 
     model = tf.models.clone_model(net)
     model.set_weights(w_new)
     return model
+'''
 
 def suvat(s0, v0, a0, dt):
-    s = s0 + v0*dt + 0.5*a0*dt**2
-    v = v0 + a0*dt
+    s = s0 + v0 * dt + 0.5 * a0 * dt ** 2
+    v = v0 + a0 * dt
     return [s, v]
 
-def game_init(character_size, screen_size):
-    pygame.init()
-    [width, height] = screen_size
-    [char_width, char_height] = character_size
-    win = pygame.display.set_mode((width,height))
-    pygame.display.set_caption("AI drone")
-    original_drone = pygame.image.load('drone.png')
-    unrotated_drone = pygame.transform.scale(original_drone, (char_width,char_height))
-    target_dot = pygame.image.load("red_dot.png")
-    target_dot = pygame.transform.scale(target_dot, (200,200))
-    sprites = [unrotated_drone, target_dot]
-    return [sprites, win]
 
-def game_update(character_size, screen_size, positions, target, sprites, win, scores):
-    [unrotated_drone, target_dot] = sprites
-    [x_new, y_new, th_new] = positions
-    [target_x, target_y] = target
-    rotated_image = pygame.transform.rotate(unrotated_drone, th_new*180/np.pi)
-    [width, height] = screen_size
-    [char_width, char_height] = character_size
-    win.fill([255,255,255])
-    win.blit(rotated_image, ((width-(char_width*abs(np.cos(th_new)) + char_height*abs(np.sin(th_new))))/2 + x_new, (height-(char_width*abs(np.sin(th_new)) + char_height*abs(np.cos(th_new))))/2 - y_new))
-    win.blit(target_dot, ((width-200)/2+target_x,(height-200)/2-target_y))
-    my_font = pygame.font.SysFont('Comic Sans MS', 30)
-    text_surface = my_font.render('Score: '+ str(scores[-1]), False, (0, 0, 0))
-    win.blit(text_surface, (0,0))
-    pygame.display.update()
-    events = pygame.event.get()
-    stop = False
-    for event in events:
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                stop = True
-        return stop
+class Drone:
+    def __init__(self, number, state, targets):
+        self.number = number
+        [self.position, self.velocity, self.acceleration] = state
+        self.brain = createNet([6,8,8,2], "tanh")
+        self.targets = targets
+        self.dt = 0.1
+        self.dist = Drone.get_dist(self)
+        self.max_thrust = 100
+        self.radius = .215/2
+        self.I = 0.266
+        self.mass = 1.38
+        self.g = -100
+        self.alive = True
 
-def pause(scores, mutation_rates, mustBreak):
-    user_choice = input("What would you like to do? \n change mutation rates[1] \n plot current scores[2] \n set breaking condition[3] \n")
-    if user_choice == "1":
-        mutation_rates = []
-        while True:
-            user_input = input("input mutation rate (~ to break): ")
-            if user_input == '~':
-                break
-            mutation_rates.append(float(user_input))
-        prob = [1 / len(mutation_rates) for i in range(len(mutation_rates))]
-
-    elif user_choice == "2":
-        plt.figure("scores")
-        plt.plot(scores)
-        plt.show()
-
-    elif user_choice == "3":
-        user_input = int(input("set break condition [1/0]: "))
-        if user_input > 0.5:
-            mustBreak = True
-        else:
-            mustBreak = False
-
-    return [mutation_rates, mustBreak]
-
-
-def run_test(net, initial_conditions, target, max_counter, dt, update_inputs, must_break, scores):
-    [character_size, screen_size, sprites, win] = update_inputs
-    [iTarget, target_counter] = [0, 0] # iTarget will get increased when target counter is triggered. Target counter is to count how long drone is at target
-    [x_target, y_target] = target[iTarget]
-    L = []
-    R = []
-
-    # Environmental constants
-    pi = np.pi
-    g = -100
-    mass = 1.38 #[kg]
-    length = .215 # [m]
-    I = 50*(mass*length**2)/12
-    M = 0 #[Nm]
-    r = length/2
-    max_thrust = 500 # [Newtons]
-
-    # Initial conditions
-    [[x0, vx0, ax0,], [y0, vy0, ay0,], [th0, om0, alp0]] = initial_conditions
-    position_state = [[x0], [y0], [th0]]
-    velocity_state = [[vx0], [vy0], [om0]]
-    acceleration_state = [[ax0], [ay0], [alp0]]
-    counter = 0
-
-    # First update
-    [x_new, vx_new] = suvat(x0, vx0, ax0, dt)
-    [y_new, vy_new] = suvat(y0, vy0, ay0, dt)
-    [th_new, om_new] = suvat(th0, om0, alp0, dt)
-    for i in range(3): position_state[i].append([x_new, y_new, th_new][i])
-    for i in range(3): velocity_state[i].append([vx_new, vy_new, om_new][i])
-
-    # Scoring variables
-    dist = [(x_target**2 + y_target**2)**0.5]
-    vel = []
-    omega = []
-    theta = []
-    x_dist = x_target - position_state[0][-1]
-    y_dist = y_target - position_state[1][-1]
-    dist.append((x_dist ** 2 + y_dist ** 2) ** .5)
-    d0 = dist[-1]
-    a = np.log(10)/(d0**2)
-    
-    stop_angle = 90*pi/180
-    while abs(x_new) < 400/2 and abs(y_new) < 400/2 and counter < max_counter and abs(th_new) < stop_angle:
-
-        # Brain inputs:
-        x_dist = x_target - position_state[0][-1]
-        y_dist = y_target - position_state[1][-1]
-        dist.append((x_dist**2 + y_dist**2)**.5)
-
-        net_inputs = [[x_dist, y_dist, vx_new, vy_new, np.sin(th_new), om_new]]
-
+    def mutate(self, r):
+        self.brain = mutateNet(self.brain, r)
+    def get_dist(self):
+        return ((self.targets[0][0] - self.position[0])**2 + (self.targets[0][1] - self.position[1])**2)**0.5
+    def update_state(self):
+        net_inputs = [[element for sublist in [self.position,self.velocity] for element in sublist]]
         # add noise
-        for i in range(len(net_inputs[0])):
-            net_inputs[0][i] = net_inputs[0][i] + np.random.rand()*net_inputs[0][i]*0.0001
 
-        [[L_thrust, R_thrust]] = 0.5 + 0.5*net.predict(net_inputs, verbose=0) #percentage of thrust
-        L_thrust = min(L_thrust*max_thrust/2,max_thrust/2)
-        R_thrust = min(R_thrust*max_thrust/2,max_thrust/2)
+        for i in range(len(net_inputs[0])):
+            net_inputs[0][i] = net_inputs[0][i] + np.random.rand() * net_inputs[0][i] * 0.01
+
+        [[L_thrust, R_thrust]] = self.max_thrust*(1 + self.brain.predict(net_inputs, verbose=0))  # percentage of thrust
 
         F = (L_thrust + R_thrust)
-        M = (-L_thrust + R_thrust)*r
-        L.append(L_thrust)
-        R.append(R_thrust)
+        M = (-L_thrust + R_thrust) * self.radius
 
-        # Update new
-        # Angular
-        alp = M/I
-        [th_new, om_new] = suvat(th_new, om_new, alp, dt)
-        omega.append(om_new)
-        theta.append(th_new)
+        self.acceleration[2] = M / self.I
+        [self.position[2], self.velocity[2]] = suvat(self.position[2], self.velocity[2], self.acceleration[2], self.dt)
 
         # x
-        Fx = -F*np.sin(th_new)
-        ax = Fx/mass
-        [x_new, vx_new] = suvat(x_new, vx_new, ax, dt)
+        Fx = -F * np.sin(self.position[2])
+        self.acceleration[0] = Fx / self.mass
+        [self.position[0], self.velocity[0]] = suvat(self.position[0], self.velocity[0], self.acceleration[0], self.dt)
 
-        # y 
-        Fy = F*np.cos(th_new) + g*mass
-        ay = Fy/mass
-        [y_new, vy_new] = suvat(y_new, vy_new, ay, dt)
+        # y
+        Fy = F * np.cos(self.position[2]) + self.g * self.mass
+        self.acceleration[1] = Fy / self.mass
+        [self.position[1], self.velocity[1]] = suvat(self.position[1], self.velocity[1], self.acceleration[1], self.dt)
+    def kill(self):
+        self.alive = False
+    def set_state(self,state):
+        [self.position, self.velocity, self.acceleration] = state
 
-        vel.append((vx_new**2 + vy_new**2)**0.5)
+class Game():
+    def __init__(self, screen_size, character_size):
+        [self.w, self.h] = screen_size
+        [self.char_w, self.char_h] = character_size
+        pygame.init()
+        self.win = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption("AI drone")
+        self.im_drone = pygame.transform.scale(pygame.image.load('drone.png'), (self.char_w, self.char_h))
+        self.im_target = pygame.transform.scale(pygame.image.load("red_dot.png"), (200, 200))
+        self.scores = []
+        self.r = 1
+        self.break_angle = 45
+        self.r_auto = True
+        self.best_weights = 0
+        self.targets = [0,0]
+        self.drones = 0
 
-        # Update old
-        for i in range(3): position_state[i].append([x_new, y_new, th_new][i])
-        for i in range(3): velocity_state[i].append([vx_new, vy_new, om_new][i])
-        for i in range(3): acceleration_state[i].append([ax, ay, alp][i])
+    def update(self, drones):
+        self.win.fill([255, 255, 255])
+        [target_x, target_y] = drones[0].targets[0]
+        for i in range(len(drones)):
+            if drones[i].alive:
+                [x_new, y_new, th_new] = drones[i].position
+                rotated_image = pygame.transform.rotate(self.im_drone, th_new * 180 / 3.14159265359)
+                self.win.blit(rotated_image, (
+                (self.w - (self.char_w * abs(np.cos(th_new)) + self.char_h * abs(np.sin(th_new)))) / 2 + x_new,
+                (self.h - (self.char_w * abs(np.sin(th_new)) + self.char_h * abs(np.cos(th_new)))) / 2 - y_new))
+        self.win.blit(self.im_target, ((self.w - 200) / 2 + target_x, (self.h - 200) / 2 - target_y))
 
-        stop = game_update(character_size, screen_size, [x_new, y_new, th_new], target[iTarget], sprites, win, scores)
-        if stop == True:
-            break
+    def pause(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    plt.plot(self.scores)
+                    plt.show()
+                elif event.key == pygame.K_r:
+                    self.r_auto = not self.r_auto
+                    print("Auto mutation rate is set to ", self.r_auto)
+                elif event.key == pygame.K_UP:
+                    self.break_angle += 5
+                    print("Break angle = ", self.break_angle)
+                elif event.key == pygame.K_DOWN:
+                    self.break_angle -= 5
+                    print("Break angle = ", self.break_angle)
+                elif event.key == pygame.K_LEFT:
+                    if not self.r_auto:
+                        self.r -= 0.0001
+                        self.r = min(1,self.r)
+                        print("r = ", self.r)
+                elif event.key == pygame.K_RIGHT:
+                    if not self.r_auto:
+                        self.r += 0.0001
+                        self.r = min(1,self.r)
+                        print("r = ", self.r)
+                elif event.key == pygame.K_m:
+                    if not self.r_auto:
+                        self.r = float(input("set mutation rate"))
+                        self.r = min(1,self.r)
+                        print("r = ", self.r)
 
-        counter +=1
-        if dist[-1] > dist[-2] and must_break:
-            break
-        if dist[-1] < 20:
-            target_counter += 1
-            if target_counter > 20:
-                iTarget +=1
-                target_counter = 0
-                [x_target, y_target] = target[iTarget]
-                x_dist = x_target - position_state[0][-1]
-                y_dist = y_target - position_state[1][-1]
-                dist.append((x_dist ** 2 + y_dist ** 2) ** .5)
-                d0 = dist[-1]
-                a = np.log(10)/(d0**2)
-        else:
-            target_counter = 0
-            
-    #score = counter/np.mean(dist)   + np.exp(-np.mean(omega)**2) #+ 1/np.mean(vel)
 
-    #for i in range(len(omega)):
-    #    score += + np.exp(-np.mean(omega)**2)
-        score = 1/(1+np.mean(dist)) + 1/(1+np.mean(dist)+abs(np.mean(omega)) )
-    return [score, L, R, stop]
 
-# surprise
+
+def run_test(game, drones, max_score):
+
+    run = True
+    pi = np.pi
+    dist = [[drones[i].get_dist()] for i in range(len(drones))]
+    omega = [[drones[i].get_dist()] for i in range(len(drones))]
+    dist0 = dist[0][0]
+    while run:
+        for i in range(len(drones)):
+            if drones[i].alive:
+                game.pause()
+                dist[i].append(drones[i].get_dist())
+                omega[i].append(drones[i].velocity[2])
+                drones[i].update_state()
+                if max_score < 0.009:
+                    if dist[i][-1] > dist[i][-2]:
+                        drones[i].kill()
+
+                if abs(drones[i].position[2]) > game.break_angle*pi/180 or dist[i][-1] > 200:
+                    drones[i].kill()
+
+        game.update(drones)
+        pygame.display.update()
+
+        if all([not drones[i].alive for i in range(len(drones))]):
+            run = False
+
+    scores = [1/(1 + np.mean(dist[i]) + abs(np.mean(omega[i]))) for i in range(len(drones))]
+    max_index, max_score = max(enumerate(scores), key=lambda x: x[1])
+
+    if game.r_auto:
+        game.r = min(1,0.1*np.mean(dist[max_index])/dist0)**2
+    return [max_index, max_score]
--- training.py
+++ training.py
@@ -1,61 +1,34 @@
 import AIDroneLib as lib
-import tensorflow.keras as tf
 import matplotlib.pyplot as plt
-import pygame
-import numpy as np
-import copy
 # Initialising the game
 character_size = [64, 64]
 screen_size = [1500, 790]
-[sprites, win] = lib.game_init(character_size, screen_size)
-pygame.font.init()
+game = lib.Game(screen_size, character_size)
 
-# Inputs needed for updating the game screen
-update_inputs = [character_size, screen_size, sprites, win]
-
-# initial parameters
-
-net = lib.createNet([6,8,8,2], "tanh")
-#net = tf.models.load_model('n')
-initial_conditions = [[0,0,0] , [0,0,0] , [0,0,0]]
-target = [[100,40], [100,100], [50, 40], [30,30], [-30,100]]
-max_counter = 100000
-dt = 0.05
+targets = [[1,10],[0,60]]
+drones = [lib.Drone(i,[[0,0,0],[0,0,0],[0,0,0]],targets) for i in range(10)]
 scores = [0]
+max_score = 0
+#r = [0.2,0.01]
+#p = [1/len(r) for i in range(len(r))]
 
-mutation_rates = [0.2,0.02]
-#mutation_rates = [0.01,0.001]
-prob = [1/len(mutation_rates) for i in range(len(mutation_rates))]
-r = 1 #mutation_rates[0]
-no_progress = -1 # Counts how many iterations had no progress
-mustBreak = True
-
-runs = 0
+tests = 0
 while True:
-    mutant = lib.mutateNet(net, r)
-    [new_score, L, R, stop] = lib.run_test(mutant, initial_conditions, target, max_counter, dt, update_inputs, mustBreak, scores)
-    if new_score > scores[-1]:
-        mutant.save('m')
-        net = copy.copy(mutant)
-        scores.append(new_score)
-        no_progress = -1
-    else:
-        scores.append(scores[-1])
+    tests += 1
+    [max_index, max_score] = lib.run_test(game, drones, max_score)
+    best = drones[max_index].brain.get_weights()
+    #r = min(1, 0.001*(max_score**(-1)-1))
+    for i in range(len(drones)):
+        drones[i].alive = True
+        drones[i].set_state([[0,0,0],[0,0,0],[0,0,0]])
+        drones[i].brain.set_weights(best)
 
-    if no_progress > 3 :
-        r = np.random.choice(mutation_rates,1,p=prob)[0]
+    if max_score > max(scores):
+        drones[0].brain.save('m')
 
-    runs+=1
-    print(r, "  current score is: ", new_score, "   runs: ", runs)
-    no_progress += 1
+    for j in range(len(drones)):
+        drones[j].mutate(game.r)
 
-    if stop == True:
-        [mutation_rates, mustBreak] = lib.pause(scores, mutation_rates, mustBreak)
-        prob = [1/len(mutation_rates) for i in range(len(mutation_rates))]
-        
-pygame.quit()
-plt.figure(1)
-plt.plot(scores)
-plt.show()
-plt.figure(2)
-print(net.get_weights())
+    game.scores.append(max_score)
+
+    print(tests,'r = ', game.r, ' max score = ', max_score)
